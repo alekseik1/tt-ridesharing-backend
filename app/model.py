3aa2f38faadd0f9a8507d228ca579ef6247dc8a8
alekseik1 <1alekseik1@gmail.com>
2019-09-28 19:34:13 +0300

    Add 'Driver' to ORM

--- app/model.py
+++ app/model.py
@@ -6,4 +6,14 @@ class User(db.Model):
     name = db.Column(db.String(40))
     surname = db.Column(db.String(40))
     email = db.Column(db.String(256))
-    photo_url = db.Column(db.String(2000))
+    photo = db.Column(db.String(2000))
+    is_trusful = db.Column(db.Boolean)
+
+
+class Driver(User):
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    passport_1 = db.Column(db.String(2000))
+    passport_2 = db.Column(db.String(2000))
+    passport_selfie = db.Column(db.String(2000))
+    driver_license_1 = db.Column(db.String(2000))
+    driver_license_2 = db.Column(db.String(2000))
