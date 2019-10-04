731ebb37eb72d4b5b1d957283c703a72a691b30d
alekseik1 <1alekseik1@gmail.com>
2019-10-04 18:25:38 +0300

    ref: constants to explicit variable

--- app/model.py
+++ app/model.py
@@ -1,13 +1,18 @@
 from app import db
 
+MAX_NAME_LENGTH = 40
+MAX_SURNAME_LENGTH = 40
+MAX_EMAIL_LENGTH = 256
+MAX_URL_LENGTH = 2000
+
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40))
-    surname = db.Column(db.String(40))
-    email = db.Column(db.String(256))
-    photo = db.Column(db.String(2000))
+    name = db.Column(db.String(MAX_NAME_LENGTH), nullable=False)
+    surname = db.Column(db.String(MAX_SURNAME_LENGTH), nullable=False)
+    email = db.Column(db.String(MAX_EMAIL_LENGTH), nullable=False)
+    photo = db.Column(db.String(MAX_URL_LENGTH))
     is_trusful = db.Column(db.Boolean)
 
     # For some reason, `User` database creation happens twice.
@@ -17,11 +22,11 @@ class User(db.Model):
 
 class Driver(User):
     id = db.Column(db.Integer, db.ForeignKey(User.__tablename__ + '.id'), primary_key=True)
-    passport_1 = db.Column(db.String(2000))
-    passport_2 = db.Column(db.String(2000))
-    passport_selfie = db.Column(db.String(2000))
-    driver_license_1 = db.Column(db.String(2000))
-    driver_license_2 = db.Column(db.String(2000))
+    passport_1 = db.Column(db.String(MAX_URL_LENGTH))
+    passport_2 = db.Column(db.String(MAX_URL_LENGTH))
+    passport_selfie = db.Column(db.String(MAX_URL_LENGTH))
+    driver_license_1 = db.Column(db.String(MAX_URL_LENGTH))
+    driver_license_2 = db.Column(db.String(MAX_URL_LENGTH))
 
     # For some reason, `Driver` database creation happens twice.
     # I cannot locate with bug, so I'm just leaving a hack here :)
