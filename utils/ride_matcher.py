fa41e7d81d75a0edad47d2c1647bd095ec318252
alekseik1 <1alekseik1@gmail.com>
2019-10-26 01:18:23 +0300

    fix: match rides by start organization and form a top by destination point distance

--- utils/ride_matcher.py
+++ utils/ride_matcher.py
@@ -6,14 +6,18 @@ from geopy.distance import great_circle
 
 # TODO: for now, `organizations` is ignored
 # TODO: Optimization! Don't take all rides, use more filters
-def find_best_rides(gps, organizations=[]):
-    all_rides = db.session.query(Ride).filter_by(is_available=True).all()
+def find_best_rides(start_organization_id, destination_gps):
+    # Get all rides starting from exact organization and are available
+    all_rides = db.session.query(Ride)\
+        .filter_by(start_organization_id=start_organization_id)\
+        .filter_by(is_available=True).all()
     # For each ride, find distance to `gps`
+    # We make top based on  destination point
     result_top = []
     for ride in all_rides:
         distance = great_circle(
-            (ride.start_organization.latitude, ride.start_organization.longitude),
-            gps
+            (ride.stop_latitude, ride.stop_longitude),
+            destination_gps
         ).kilometers
         result_top.append((ride, distance))
     result_top.sort(key=operator.itemgetter(1))
