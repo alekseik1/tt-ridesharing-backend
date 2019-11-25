44a182a436490f6a17ac2bd0fdc45b77ee4faf7e
alekseik1 <1alekseik1@gmail.com>
2019-11-26 02:19:37 +0300

    fix: don't leave ride if user is not in it

--- main_app/views/ride.py
+++ main_app/views/ride.py
@@ -181,11 +181,14 @@ def leave_ride():
         error = build_error(SwaggerResponses.INVALID_RIDE_WITH_ID, ride_id)
         return jsonify(error), 400
     # Иначе, убираем пользователя из поездки
-    ride.passengers.remove(current_user)
-    # Обновим поездку
-    ride.is_available = True
-    db.session.commit()
-    response = SwaggerResponses.RIDE_ID
+    is_removed = False
+    if current_user in ride.passengers:
+        ride.passengers.remove(current_user)
+        is_removed = True
+        # Обновим поездку
+        ride.is_available = True
+        db.session.commit()
+    response = dict(is_user_removed=is_removed)
     return jsonify(response), 200
 
 
