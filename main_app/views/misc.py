aef215a638573f4b8f0c15bd401ff1c14064e38b
alekseik1 <1alekseik1@gmail.com>
2020-03-16 23:20:30 +0300

    ref: cleanup for geocoding

--- main_app/views/misc.py
+++ main_app/views/misc.py
@@ -20,25 +20,30 @@ FORWARD_GEOCODING_URL = 'https://geocode-maps.yandex.ru/1.x/?apikey={key}&bbox=3
 GEO_TOKEN = os.environ['GEOCODING_KEY']
 
 
+def _parse_geocoding_results(response):
+    return [dict(
+        address=x['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['formatted'],
+        # NOTE: (latitude, longitude) are in reverse order
+        gps=dict(zip(
+            ('latitude', 'longitude'),
+            x['GeoObject']['Point']['pos'].split(' ')[::-1]))
+    ) for x in response['response']['GeoObjectCollection']['featureMember']]
+
+
 def reverse_geocoding_blocking(latitude, longitude):
-    return requests.get(
+    yandex_response = requests.get(
         REVERSE_GEOCODING_URL.format(key=GEO_TOKEN, latitude=latitude, longitude=longitude)
     ).json()
+    results = _parse_geocoding_results(yandex_response)
+    # NOTE: we assume that first list element is nearest element
+    return jsonify(results[0])
 
 
 def forward_geocoding_blocking(address):
     yandex_response = requests.get(
         FORWARD_GEOCODING_URL.format(key=GEO_TOKEN, address=address)
     ).json()
-    results = [
-        dict(
-            address=x['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['formatted'],
-            # NOTE: (latitude, longitude) are in reverse order
-            gps=dict(zip(
-                ('latitude', 'longitude'),
-                x['GeoObject']['Point']['pos'].split(' ')[::-1]))
-        )
-        for x in yandex_response['response']['GeoObjectCollection']['featureMember']]
+    results = _parse_geocoding_results(yandex_response)
     return jsonify(results)
 
 
