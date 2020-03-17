b93c62b16d582da317c512d43991b8115bf08358
alekseik1 <1alekseik1@gmail.com>
2020-03-17 22:08:25 +0300

    feat: Sentry SQLAlchemy integration

--- app.py
+++ app.py
@@ -23,9 +23,10 @@ def create_app():
     if 'SENTRY_DSN' in os.environ:
         import sentry_sdk
         from sentry_sdk.integrations.flask import FlaskIntegration
+        from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
         sentry_sdk.init(
             dsn=os.environ['SENTRY_DSN'],
-            integrations=[FlaskIntegration()]
+            integrations=[FlaskIntegration(), SqlalchemyIntegration()]
         )
     app = Flask(__name__)
 
