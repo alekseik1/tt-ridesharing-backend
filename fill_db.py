493638e8f196a332b463347a384980ebdfd23fab
alekseik1 <1alekseik1@gmail.com>
2020-03-26 14:47:15 +0300

    feat: `fill_db` with JoinRideRequests

--- fill_db.py
+++ fill_db.py
@@ -1,6 +1,7 @@
 import factory.random
 
 from app import create_app, db
+from main_app.fixtures.join_ride_request import JoinRideRequestFactory
 from main_app.fixtures.organizations import OrganizationFactory
 from main_app.fixtures.users import UserFactory
 from main_app.fixtures.cars import CarFactory
@@ -18,7 +19,7 @@ def fill_database(app):
         cars = factory.build_batch(CarFactory, 20)
         car_users = [x.owner for x in cars]
         db.session.add_all(cars)
-        # We will explicitly iterate since we need full randomness
+        # We will explicitly iterate since we don't want to create users for organizations
         organizations = []
         all_users = users + car_users
         for i in range(3):
@@ -26,6 +27,7 @@ def fill_database(app):
             organizations.append(OrganizationFactory(
                 users=[all_users[j] for j in range(i, len(all_users), 3)]
             ))
+        # Same for rides
         rides = []
         for i in range(5):
             rides.append(RideFactory(
@@ -34,8 +36,19 @@ def fill_database(app):
                 passengers=users[3*i:3*(i+1)],
                 car=car_users[i % len(car_users)].cars[0]
             ))
+        # User can join ride only via JoinRequest acceptance
+        # So, we need to create corresponding JoinRequest with 'ACCEPTED' answer
+        join_requests = []
+        for ride in rides:
+            for user in ride.passengers:
+                join_requests.append(JoinRideRequestFactory(
+                    ride=ride,
+                    user=user,
+                    status=1
+                ))
         db.session.add_all(organizations)
         db.session.add_all(rides)
+        db.session.add_all(join_requests)
         db.session.commit()
 
 
