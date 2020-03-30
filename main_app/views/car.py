61db5133df95d18590986cc1cbeab0b534e42f75
alekseik1 <1alekseik1@gmail.com>
2020-03-31 02:02:51 +0300

    Revert "ref: remove old schemas and endpoints"
    This reverts commit 1d07be23

--- main_app/views/car.py
+++ main_app/views/car.py
@@ -1,9 +1,11 @@
 from flask import jsonify, request
 from flask_login import login_required, current_user
 
-from main_app.schemas import CarSchema, CarPermissiveSchema, IdSchema
+from main_app.schemas import CarSchema, CarPermissiveSchema, RegisterCarForDriverSchema, IdSchema
 from app import db
+from main_app.model import Car, User
 from main_app.exceptions.custom import InsufficientPermissions
+from main_app.controller import validate_params_with_schema
 from main_app.views import api
 
 
@@ -28,3 +30,31 @@ def car():
         db.session.add(car)
         db.session.commit()
         return IdSchema().dump(car)
+
+
+@api.route('/get_my_cars', methods=['GET'])
+@login_required
+def get_my_cars():
+    car_schema = CarSchema(many=True)
+    corresponding_driver = db.session.query(User).filter_by(id=current_user.id).first()
+    return jsonify(car_schema.dump(corresponding_driver.cars))
+
+
+def register_car(car_info):
+    car = Car(**car_info)
+    db.session.add(car)
+    db.session.commit()
+    return car
+
+
+@api.route('/register_own_car', methods=['POST'])
+@login_required
+def register_car_for_driver():
+    data = request.get_json()
+    # Также валидирует на наличие водителя. Я начал писать нормально
+    error = validate_params_with_schema(RegisterCarForDriverSchema(), data)
+    if error:
+        return error
+    data['owner_id'] = current_user.id
+    car = register_car(data)
+    return jsonify(car_id=car.id), 200
--- main_app/views/misc.py
+++ main_app/views/misc.py
@@ -1,8 +1,9 @@
-from main_app.misc import reverse_geocoding_blocking, forward_geocoding_blocking
+from main_app.misc import get_distance, reverse_geocoding_blocking, forward_geocoding_blocking
 from main_app.views import api
 from main_app.schemas import ReverseGeocodingSchema, \
-    ForwardGeocodingSchema
+    ForwardGeocodingSchema, OrganizationSchemaUserIDs
 from flask import request, jsonify
+from flask_login import login_required, current_user
 
 
 @api.route('/decode_gps', methods=['POST'])
@@ -15,3 +16,19 @@ def decode_gps():
 def encode_address():
     data = ForwardGeocodingSchema().load(request.json)
     return jsonify(forward_geocoding_blocking(**data))
+
+
+@api.route('/nearest_organizations', methods=['GET'])
+@login_required
+def get_nearest_organizations():
+    data = ReverseGeocodingSchema().load(request.args)
+    # TODO: what if organization does not have coordinates??
+    result = sorted(
+        current_user.organizations,
+        key=lambda x: get_distance(
+            (x.latitude, x.longitude),
+            (float(data['latitude']), float(data['longitude']))
+        )
+    )
+    dump_schema = OrganizationSchemaUserIDs(many=True)
+    return jsonify(dump_schema.dump(result))
