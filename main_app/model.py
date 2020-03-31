b66542a253cb765b8d9c522ba22bf279ac29ec0c
alekseik1 <1alekseik1@gmail.com>
2020-03-31 19:52:02 +0300

    feat: basic elasticsearch configuration

--- main_app/model.py
+++ main_app/model.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import validates
 from main_app.misc import reverse_geocoding_blocking
 from datetime import datetime
 
+from main_app.search import query_index, add_to_index, remove_from_index
 from app import db
 from settings import MAX_EMAIL_LENGTH, MAX_NAME_LENGTH, MAX_SURNAME_LENGTH, MAX_URL_LENGTH
 
@@ -20,6 +21,49 @@ association_user_organization = db.Table(
     db.Column('left_id', db.Integer, db.ForeignKey('user.id', ondelete='cascade')),
     db.Column('right_id', db.Integer, db.ForeignKey('organization.id', ondelete='cascade'))
 )
+
+
+class SearchableMixin:
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return cls.query.filter_by(id=0), 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        return cls.query.filter(cls.id.in_(ids)).order_by(
+            db.case(when, value=cls.id)), total
+
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+
+    @classmethod
+    def reindex(cls):
+        for obj in cls.query:
+            add_to_index(cls.__tablename__, obj)
+
+
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
 
 
 class RatingMixin:
